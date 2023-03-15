0192bc6b21564e4ecf4ee49c151df2e8dc70275e
kitouni <28654657+okitouni@users.noreply.github.com>
2023-03-14 22:32:48 -0400

    added make_tree function to tree

--- 958_CheckCompletenessofaBinaryTree.py
+++ 958_CheckCompletenessofaBinaryTree.py
@@ -3,7 +3,9 @@
 # In a complete binary tree, every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
 from collections import deque
 from typing import Optional
-from tree import TreeNode
+from tree import TreeNode, make_tree
+import unittest
+
 
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
@@ -17,5 +19,26 @@ class Solution:
                     continue
                 q.append(node.left)
                 q.append(node.right)
-                if saw_none: return False
+                if saw_none:
+                    return False
         return True
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+        self.examples = [
+            ([1, 2, 3, 4, 5, 6], True),
+            ([1, 2, 3, 4, 5, None, 7], False),
+            ([1], True),
+            ([1, 3, 4, None, 5], False),
+        ]
+
+    def test_isCompleteTree(self):
+        for nodes, expected in self.examples:
+            print("testing nodes: ", nodes, "expected: ", expected)
+            self.assertEqual(self.sol.isCompleteTree(make_tree(nodes)), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tree.py
+++ tree.py
@@ -14,3 +14,23 @@ class TreeNode:
 
     def __repr__(self) -> str:
         return f"TreeNode({self.val}, {self.left}, {self.right})"
+
+
+
+def make_tree(nodes):
+    if not nodes:
+        return None
+    root = TreeNode(nodes[0])
+    queue = [root]
+    i = 1
+    while i < len(nodes):
+        node = queue.pop(0)
+        if nodes[i] is not None:
+            node.left = TreeNode(nodes[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            node.right = TreeNode(nodes[i])
+            queue.append(node.right)
+        i += 1
+    return root
